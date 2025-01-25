4d4c5ae0c24cf498f56281185245e05a71db125c
Arian Abdi <abdiarian2@gmail.com>
2025-01-25 13:05:27 +0330

    Update installation.sh

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+
+# Check if pm2 process with the name 'fxpilot-react' exists and delete it
+if pm2 list | grep -q "fouladyar-pwa"; then
+  echo "Stopping and deleting existing 'fouladyar-pwa' PM2 process..."
+  pm2 delete fouladyar-pwa
+fi
+
+# Remove the existing 'mrpropfund-react' folder if it exists
+if [ -d "fouladyar-pwa" ]; then
+  echo "Removing existing 'fouladyar-pwa' directory..."
+  rm -rf fouladyar-pwa
+fi
+
+
 # Install Node.js (if not already installed)
 if ! command -v node &>/dev/null; then
   echo "Installing Node.js..."
-  curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+  sudo sudo apt update
+  curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
   sudo apt-get install -y nodejs
 fi
 
@@ -12,41 +27,65 @@ if ! command -v jq &>/dev/null; then
   sudo apt-get install -y jq
 fi
 
-# Install NestJS CLI (if not already installed)
-if ! command -v nest &>/dev/null; then
-  echo "Installing NestJS CLI..."
-  sudo npm install -g @nestjs/cli
+# Install NGinx (if not already installed)
+if ! command -v nginx &>/dev/null; then
+  echo "Installing NGinx..."
+  sudo apt-get install nginx
 fi
 
 
-# Install NestJS CLI (if not already installed)
+# Install Pm2 (if not already installed)
 if ! command -v pm2 &>/dev/null; then
   echo "Installing pm2..."
   sudo npm install -g pm2
 fi
 
 
+# Install Pm2 (if not already installed)
+if ! command -v yarn &>/dev/null; then
+  echo "Installing pm2..."
+  sudo npm install -g yarn
+fi
+
+# Install Serve (if not already installed)
+if ! command -v serve &>/dev/null; then
+  echo "Installing serve..."
+  sudo npm install -g serve
+fi
+
+
 # Clone your GitHub repository (replace <your-github-repository-url> with your actual repository URL)
 echo "Cloning your project..."
-git clone "https://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@github.com/arianabdi/cliconsult-web.git"
+git clone "https://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@github.com/arianabdi/fouladyar-pwa.git"
+
+
 
 # Navigate to the project directory
-cd cliconsult-web
-#sudo chmod +x ./scripts/set_users.sh
-#sudo chmod +x ./scripts/reboot.sh
+cd fouladyar-pwa
 
 # Install project dependencies
 echo "Installing node modules..."
-npm install --legacy-peer-deps
+yarn install
 
 # Build the NestJS application
-echo "Building the React application..."
-npm run build
+echo "Building the Next application..."
+npm run build:webpack
+
+# Ensure the build process is complete and doesn't hang
+if [ $? -ne 0 ]; then
+  echo "Webpack build failed. Exiting installation."
+  exit 1
+fi
+
+echo "Webpack build completed successfully."
 
 # Start the application using pm2 (assuming pm2 is installed)
 echo "Starting the application"
 #npm start dist/main.js
-npm start:prod
+pm2 start app.config.json
+
+#start application after server reboot
+pm2 save
 
 #start application after server reboot
 pm2 startup
